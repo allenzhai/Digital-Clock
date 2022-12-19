16e2e694210aa19b1a9475d5d628a8c172638a68
allenzhai <allenzhai730@gmail.com>
2022-12-19 13:42:08 -0500

    creating digital clock

---
+++ clock.js
@@ -0,0 +1,81 @@
+months = [
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "Novemeber",
+    "December"
+];
+
+days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+];
+
+function getStandardHours(militaryHours) {
+    var result;
+    if (militaryHours === 0) {
+      result = 12;
+    } else if (militaryHours > 12) {
+      result = militaryHours - 12;
+    } else {
+      result = militaryHours;
+    }
+    return addLeadingZero(result);
+}
+
+function addLeadingZero(num) {
+    var displayValue = num.toString();
+    if (displayValue.length === 1) {
+        return `0${displayValue}`;
+    }
+    return displayValue;
+}
+
+function addLeadingZeros(milliseconds) {
+    var displayValue = milliseconds.toString();
+    switch (displayValue.length) {
+        case (1):
+            return `00${displayValue}`;
+        case (2):
+            return `0${displayValue}`;
+        default:
+            return displayValue;
+    }
+}
+
+function displayDateAndTime() {
+    const dateDisplay = document.getElementById("date");
+    const timeDisplay = document.getElementById("time");
+    const currentDay = new Date();
+
+    const month = months[currentDay.getMonth()];
+    const day = addLeadingZero(currentDay.getDay());
+    const weekDay = days[currentDay.getDay()];
+    const year = currentDay.getFullYear();
+
+    const militaryHours = currentDay.getHours();
+    const standardHours = getStandardHours(militaryHours);
+    const minute = addLeadingZero(currentDay.getMinutes());
+    const second = addLeadingZero(currentDay.getSeconds());
+    const miliseconds = addLeadingZeros(currentDay.getMilliseconds());
+    const meridiem = militaryHours < 12 ? "AM" : "PM";
+
+    dateDisplay.textContent = `${weekDay}, ${month} ${day}, ${year}`;
+    timeDisplay.textContent = `${standardHours}:${minute}:${second}:${miliseconds} ${meridiem}`;
+}
+
+
+setInterval(displayDateAndTime);
+displayDateAndTime();
